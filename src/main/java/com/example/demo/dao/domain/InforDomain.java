dbc26903cedbbfd1220e10dfb1df46cb469efff6
pd201608040118 <2935542210@qq.com>
2019-05-21 14:38:27 +0800

    小修改
    修改

--- src/main/java/com/example/demo/dao/domain/InforDomain.java
+++ src/main/java/com/example/demo/dao/domain/InforDomain.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class InforDomain {
     private String Title;            //标题
     private Date InforTime;          //时间
-    private String Conent;           //内容
+    private String Conent;              //内容
     private String Author;           //作者
 
     public String getConent() {
